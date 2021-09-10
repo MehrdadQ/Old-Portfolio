962217bc3ccf24022902fb0bbc51ac672cba853a
MehrdadQ <85261795+MehrdadQ@users.noreply.github.com>
2021-09-10 13:13:52 -0400

    Update home.js

--- home.js
+++ home.js
@@ -34,17 +34,37 @@ $(document).ready(function () {
 });
 
 // Fix scroll margin for anchor link when on mobile view
-var height = document.documentElement.clientWidth;
-if (height < 980) {
+var width = document.documentElement.clientWidth;
+if (width < 980) {
   document.getElementById("projects").style["scroll-margin-top"] = "250px";
   document.getElementById("contact").style["scroll-margin-top"] = "250px";
+}
+
+if (width < 557) {
+  smalls = document.querySelectorAll(".project");
+  for (var i = 0, element; (element = smalls[i]); i++) {
+    element.style["height"] = "700px";
+  }
+}
+
+if (width < 400) {
+  smalls = document.querySelectorAll(".project");
+  for (var i = 0, element; (element = smalls[i]); i++) {
+    element.style["height"] = "1000px";
+  }
+}
+
+if (width < 400) {
+  links = document.querySelectorAll(".link-container");
+  for (var i = 0, element; (element = links[i]); i++) {
+    element.style["flex-direction"] = "column";
+  }
 }
 
 function sendEmail() {
   msg = document.getElementById("msg").value;
   subject = document.getElementById("subject").value;
   to = "mehrdad.ghannad@mail.utoronto.ca";
-  console.log("llmao");
   window.open("mailto:" + to + "?subject=" + subject + "&body=" + msg);
 }
 
